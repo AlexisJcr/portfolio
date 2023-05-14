a3baefd535f8b279bdbbeeb351f1b718514321cc
AlexisJcr <128303600+AlexisJcr@users.noreply.github.com>
2023-05-14 17:08:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,60 @@
 //===== MENU BURGER =====//
-
 function burgerclic() {
+  var navContainer = document.getElementById("nav-container");
+  var burgerItems = document.querySelector(".nav-burger-items");
+
+  if (navContainer.style.height === "5rem") {
+    navContainer.style.height = "25rem";
+    burgerItems.style.display = "flex";
+  } else {
+    navContainer.style.height = "5rem";
+    burgerItems.style.display = "none";
+  }
+}
+
+function checkBurgerDisplay() {
+  var imgBurger = document.querySelector(".img-burger")
+  var navContainer = document.querySelector(".nav-container");
+  var burgerItems = document.querySelector(".nav-burger-items");
+  if (window.getComputedStyle(imgBurger).display === "none") {
+    navContainer.style.height = "5em";
+    burgerItems.style.display = "none";
+  }
+}
+
+// Vérifier l'état initial au chargement de la page
+window.addEventListener("load", checkBurgerDisplay);
+
+// Vérifier l'état à chaque changement de l'état d'affichage de l'élément "img-burger"
+var observer = new MutationObserver(checkBurgerDisplay);
+observer.observe(document.querySelector(".img-burger"), { attributes: true });
+
+// Vérifier l'état à chaque fois que la taille de la fenêtre est modifiée
+window.addEventListener("resize", checkBurgerDisplay);
+
+
+
+
+
+
+/*function burgerclic() {
     var navContainer = document.querySelector(".nav-container");
     var burgerItems = document.querySelector(".nav-burger-items");
-    if (navContainer.style.height === "60px")
+    if (navContainer.style.height === "5rem")
     {
       navContainer.style.height = "8em";
       burgerItems.style.display = "flex";
   
     }
-    else{
-      navContainer.style.height = "5em";
-      burgerItems.style.display = "none";
-    }
-}
+    //else{
+      //navContainer.style.height = "5em";
+      //burgerItems.style.display = "none";
+    //}
+}*/
   
-  function checkBurgerDisplay() {
-    var imgBurger = document.querySelector(".img-burger")
-    var navContainer = document.querySelector(".nav-container");
-    var burgerItems = document.querySelector(".nav-burger-items");
-    if (window.getComputedStyle(imgBurger).display === "none") {
-      navContainer.style.height = "5em";
-      burgerItems.style.display = "none";
-    }
-}
-  
-  // Vérifier l'état initial au chargement de la page
-  window.addEventListener("load", checkBurgerDisplay);
-  
-  // Vérifier l'état à chaque changement de l'état d'affichage de l'élément "img-burger"
-  var observer = new MutationObserver(checkBurgerDisplay);
-  observer.observe(document.querySelector(".img-burger"), { attributes: true });
-  
-  // Vérifier l'état à chaque fois que la taille de la fenêtre est modifiée
-  window.addEventListener("resize", checkBurgerDisplay);
 
-/* === SLIDER ===*/
+
+
 
 
 
